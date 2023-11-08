d617e2e4992e93539fc7bef97c39c9b1f9ed78e4
jensmoes <jens@troest.com>
2023-11-08 10:07:57 -0800

    Move Array extension into separate file

--- Sources/Autograph/Array+Path.swift
+++ Sources/Autograph/Array+Path.swift
@@ -8,4 +8,24 @@
 
 
 import Foundation
-
+import SwiftUI
+extension Array where Element == [CGPoint] {
+    func path(in canvas: CGSize) -> Path {
+        var result = Path()
+        // If there are none to return
+        if isEmpty {return result}
+        
+        let _ = forEach({ stroke in
+            guard let firstPoint = stroke.first else { return }
+            // Move to the first point of each stroke
+            result.move(to: firstPoint)
+            // Insert the rest
+            stroke[1...].forEach { point in
+                result.addLine(to: point)
+            }
+        })
+        
+        let transform = CGAffineTransform(scaleX: canvas.width, y: canvas.height)
+        return result.applying(transform)
+    }
+}
--- Sources/Autograph/Autograph.swift
+++ Sources/Autograph/Autograph.swift
@@ -86,27 +86,6 @@ public struct Autograph<T: Autographic>: View {
 
 }
 
-extension Array where Element == [CGPoint] {
-    func path(in canvas: CGSize) -> Path {
-        var result = Path()
-        // If there are none to return
-        if isEmpty {return result}
-        
-        let _ = forEach({ stroke in
-            guard let firstPoint = stroke.first else { return }
-            // Move to the first point of each stroke
-            result.move(to: firstPoint)
-            // Insert the rest
-            stroke[1...].forEach { point in
-                result.addLine(to: point)
-            }
-        })
-        
-        let transform = CGAffineTransform(scaleX: canvas.width, y: canvas.height)
-        return result.applying(transform)
-    }
-}
-
 fileprivate struct PennedShape: Shape {
     /// The strokes
     let data: [[CGPoint]]
